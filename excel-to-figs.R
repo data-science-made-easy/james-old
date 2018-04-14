e221c7c964fea7fbb7f1dc86b0a38ac5bc236626
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2018-04-14 10:41:01 +0200

    Added new example + cleaned code.

--- excel-to-figs.R
+++ excel-to-figs.R
@@ -10,58 +10,26 @@ source("james-light.R")
 source("james-figures.R")
 source("import-xls-functions.R")
 
-doc_name  <- "cep2018"
-scenario  <- "2018_03_21_FINAL"
-REMOTE    <- T
+doc_name  <- DEFAULT
+scenario  <- DEFAULT
 READ_DATA <- T
 MAKE_FIGS <- T
 DATA_FILE <- F
 
-input_path_prefix <- if (REMOTE) "/Volumes/James/input/cep2018_" else "~/cpb_data/cpb_figs/input/cep2018/cep2018_"
-j_init(file_name = "cep2018_final.james", active_scenario = scenario, active_project = doc_name)
+input_path_prefix <- "data"
+j_init(file_name = paste0(input_path_prefix, "/data.james"), active_scenario = scenario, active_project = doc_name)
 
-# ##### TODO PBL >>>>>>>>>>>>>>
-# input_path_prefix <- "/Users/mdijkstra/Dropbox/cpb/presentation/PBL-SCP/data_pbl/"  # TODO PBL
-# doc_name  <- "pbl" # TODO PBL
-# scenario  <- "clo" # TODO PBL
-#
-# j_init(file_name = paste0(input_path_prefix, "pbl.james"), active_scenario = scenario, active_project = doc_name)
-# ##### <<<<<<<<<<<< TODO PBL
-
-#j_init(file_name = "cep2018_final.james", active_scenario = scenario, active_project = doc_name)
 if (READ_DATA) {
-  # file_list <- c("ado", "aek", "fan", "gwm", "jcb", "jhjp", "kst", "mbvk", "mhdv", "mml", "mvk", "pmk", "sptg", "svv", "wbcs")
-  
-  # ##### ======== TODO PBL
-  # file_list <- "james_pbl" # TODO PBL
-  # ##### ======== TODO PBL
-  
-  # file_list <- "svv_laura"
-  file_list <- "aek"
-  #file_list <- "kst" #kst_20180303
-  #file_list <- "gwm_md"
-  #file_list <- "AD_mbvk"
-  #file_list <- "jhjp"
-  # EMAIL paste0(paste0(file_list, "@cpb.nl"), collapse= "; ")
+  file_list <- "aad"
   for (user_file in file_list) { # , 
-    print(user_file)
-    #file_name = "/Users/mdijkstra/Dropbox/cpb/projects/ramingen_db/cep2018/cep2018_james.xlsx"#"/Users/mdijkstra/Dropbox/cpb/projects/ramingen_db/data-figuren-kCEP2018.xlsx"
-    file_name = paste0(input_path_prefix, user_file, ".xlsx")#"/Users/mdijkstra/Dropbox/cpb/projects/ramingen_db/data-figuren-kCEP2018.xlsx"
+    file_name = paste0(input_path_prefix, "/", user_file, ".xlsx")
 
     if (!validate_xlsx(file_name)) print(file_name) else import_xlsx_as_ts(file_name)
   }
-  if (!all(1 == as.numeric(j_ls()$version))) warning("Identieke bestandsnamen (figs)?")
 }
 
-if (REMOTE) {
-  path_base  <- "/Volumes/James/output"
-} else {
-  path_base  <- paste0("~/cpb_data/cpb_figs/output/cep2018_", format(Sys.time(), "%Y-%m-%d-%H%M"))#tempdir()
-  # ##### ======== TODO PBL
-  # path_base <- input_path_prefix # TODO PBL
-  # ##### ======== TODO PBL
-  system(paste("mkdir -p", path_base))
-}
+path_base  <- paste0(input_path_prefix, "/output")#tempdir()
+system(paste("mkdir -p", path_base))
 
 if (MAKE_FIGS) {
   path_pdf  <- paste0(path_base, "/pdf")
