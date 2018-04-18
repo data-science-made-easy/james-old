c4fe2f535bb3e654ddf4def901671cf3018bcb37
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2018-04-18 15:56:53 +0200

    j_put returns index

--- R/j_put.R
+++ R/j_put.R
@@ -7,21 +7,21 @@
 #' @param doc document your data with plain text or Markdown
 #' @param scenario the scenario (default active scenario)
 #' @param project project to which x belongs (default active project)
-#' @param ignore_if_duplicate (default TRUE)
+#' @param add_if_duplicate (default TRUE)
 #' @param activate_project_scenario the given project and scenario become active after storing x (default TRUE), which sets defaults for the next time you want to get/put data
 #'
-#' @return version if successfully added, else FALSE
+#' @return index if successfully added, else NULL. Both invisibly.
 #'
 #' @seealso \code{\link{j_get}}, \code{\link{j_ls}}, \code{\link{j_init}}
 #'
 #' @export
 
-j_put <- function(x, type = "", doc = NA, scenario = james.env$j_root$active_scenario, project = james.env$j_root$active_project, ignore_if_duplicate = TRUE, activate_project_scenario = TRUE) {
+j_put <- function(x, type = "", doc = NA, scenario = james.env$j_root$active_scenario, project = james.env$j_root$active_project, add_if_duplicate = TRUE, activate_project_scenario = TRUE) {
   james_initialise()
   
   # First check if we really want to add x
   x2_object <- j_get(type = type, scenario = scenario, project = project, what = "object")
-  add_x     <- !ignore_if_duplicate || is.null(x2_object)
+  add_x     <- add_if_duplicate || is.null(x2_object)
   if (!add_x) add_x <- !identical(x, x2_object$data) || !identical(doc, x2_object$doc)
   
   if (add_x) {
@@ -34,6 +34,7 @@ j_put <- function(x, type = "", doc = NA, scenario = james.env$j_root$active_sce
       jdata$doc <- x2_object$doc # Re-use doc
     }
     james.env$j_root$data_lst <- append(james.env$j_root$data_lst, jdata) # Add
+    index <- length(james.env$j_root$data_lst)
     
     # Select that project and scenario
     if (activate_project_scenario) {
@@ -41,8 +42,8 @@ j_put <- function(x, type = "", doc = NA, scenario = james.env$j_root$active_sce
       james.env$j_root$active_scenario <- scenario    
     }
   } else {
-    version <- FALSE
+    index <- NULL
   }
   
-  return(invisible(version))
+  return(invisible(index))
 }
