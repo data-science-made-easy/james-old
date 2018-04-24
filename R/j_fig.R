6c4a751a1bad56ad282498df5284c1a01fa960b1
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2018-04-24 15:55:44 +0200

    In j_fig, param meta can temp. overrule other meta data

--- R/j_fig.R
+++ R/j_fig.R
@@ -3,12 +3,30 @@
 #' Plot the data at position 'index' in j_ls(), given its meta data
 #'
 #' @param index id of data you want to plot
+#' @param meta settings you want to use for your figure. These settings are not stored and will be used only once.
+#' @param file_name file name
 #'
 #' @return None
 #'
-#' @export
+#' @export j_fig
 
-j_fig <- function(index) {
-  x <- j_get(index)
-  plot(x)
+j_fig <- function(index, meta) {
+  x <- j_get(index, what = "data")
+  m <- j_get(index, what = "meta")
+  
+  
+  
+  # Get x-axis
+  #x_at
+  #x_at_lab
+  
+    # 1 str_replace_all(t_vec_labels, "\\\\n", "\n")
+    # 2 chartr(".", get_settings()$decimal_sep, as.character(t_vec_labels))    
+    # 3 Give x_at_lab same length as x_at
+  
+  # Remove x-axis
+  x <- x[, -1, drop = FALSE]
+  
+  # Use series_type from Excel or set defaults
+  # ?Get color scheme
 }
