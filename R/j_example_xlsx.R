249c70a80a7aa1daf433cd5762532cb2e8dd57e0
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2018-04-26 18:28:52 +0200

    Added function to generate example xlsx

--- R/j_example_xlsx.R
+++ R/j_example_xlsx.R
@@ -2,12 +2,12 @@
 #'
 #' You can use this example to import, analyse, and visualise your own data
 #'
-#' @param file_name file_name where your xlsx-file should be stored
+#' @param file_name file_name where your xlsx-file should be stored (default temporary xlsx-file)
 #'
 #' @return None
 #' 
 #' @export
 
-j_example_xlsx <- function(file_name) {
-  
+j_example_xlsx <- function(file_name = paste0(tempfile(), ".xlsx")) {
+  file.copy(from = system.file("extdata", "james_example.xlsx", package = "james"), to = file_name)
 }
