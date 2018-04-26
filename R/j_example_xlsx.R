6cc6435c57f8544fed3f986b0794c9970284b8ff
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2018-04-26 14:57:18 +0200

    Function to produce xlsx-file that can be imported in James; users can use this as a starting point to import own data

---
+++ R/j_example_xlsx.R
@@ -0,0 +1,13 @@
+#' Get example xlsx file
+#'
+#' You can use this example to import, analyse, and visualise your own data
+#'
+#' @param file_name file_name where your xlsx-file should be stored
+#'
+#' @return None
+#' 
+#' @export
+
+j_example_xlsx <- function(file_name) {
+  
+}
