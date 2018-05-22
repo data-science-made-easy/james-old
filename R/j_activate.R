17c19c409a9ab36668ae644a2ef08c62aead4772
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2018-05-22 09:41:47 +0200

    fix doc

--- R/j_activate.R
+++ R/j_activate.R
@@ -1,7 +1,5 @@
 #' Activate a certain project and scenario
 #'
-#' j_ls() lists only data from the active project and scenario. If you change project and/or scenario, you can use this function to activate it/them. Use unique(j_ls(active_project_scenario_only=F)[c("project", "scenario")]) to list all unique combinations of the two.
-#'
 #' @param type the data type you want to activate
 #' @param scenario the scenario you want to activate
 #' @param project the project you want to activate
