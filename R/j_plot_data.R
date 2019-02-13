9a8bf914e940e03c2b9651414caadaaf0391b528
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2019-02-13 14:06:39 +0100

    Fixed bug when just plotting data (type = tmp now)

--- R/j_plot_data.R
+++ R/j_plot_data.R
@@ -1,7 +1,7 @@
 #' @export j_plot_data
 
 j_plot_data <- function(data, meta = list()) {
-  index <- j_put(data)
+  index <- j_put(data, type = "tmp")
   meta <- combine_lists(high_prio = combine_lists(high_prio = meta, low_prio = list(name = class(data))), low_prio = j_import_settings(meta = meta))
   j_plot(index, meta)
 }
