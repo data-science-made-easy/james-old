9331de649a8e8a884d246e66a5c4674dbee7d5e4
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2019-02-22 11:36:16 +0100

    Update j_start

--- inst/extdata/james-for-xlsx.R
+++ inst/extdata/james-for-xlsx.R
@@ -3,4 +3,4 @@ if (is.element("cpblib", installed.packages())) cpblib::use_cpblib()
 library(james)
 
 j_clean()
-j_plot_xlsx("james-example-multiple-tabs.xlsx")
+j_plot_xlsx("james-example.xlsx")
--- inst/extdata/james-in-your-analysis-pipeline.R
+++ inst/extdata/james-in-your-analysis-pipeline.R
@@ -7,4 +7,17 @@ mat <- cbind(2000:2009, random = runif(10), increasing = 1:10)
 
 # Create the plot
 j_clean()
-j_plot_data(data = mat, meta = list(name = "hello-world", title = "Hello world!", hline_bold = 0))
+j_plot_data(data = mat, meta = list(name = "hello-world", title = "Hello world!", series_type = c("bar--", "line"), hline_bold = 0))
+
+# Import data
+j_clean()
+j_import("james-example-one-tab.xlsx")
+
+# Check import
+j_ls()
+j_get(1)
+
+# Set some meta data
+j_set_meta(1, list(title = "Imported data"))
+j_set_meta(1, list(sub_title = "Just an example", hline_bold = 0))
+j_plot(1)
