11c9880e2d632c1068ad262b4fcf7e5ac7d99eaf
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2019-03-08 13:37:56 +0100

    deal with constants tab

--- inst/extdata/james-for-xlsx.R
+++ inst/extdata/james-for-xlsx.R
@@ -2,4 +2,4 @@ rm(list = ls())
 cpblib::use_cpblib()
 library(james)
 
-j_plot_xlsx("james-example.xlsx")
+j_plot_xlsx("james-example.xlsx", meta = list(settings_file = "james-local-settings.xlsx"))
