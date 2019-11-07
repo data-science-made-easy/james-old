b6bd3be51720cc78b34130e4d5910284f026478b
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2019-11-07 14:12:10 +0100

    fix

--- R/j_db-functions.R
+++ R/j_db-functions.R
@@ -1,6 +1,6 @@
 j_db_put <- function(meta) { # meta$d0 is data
   if (is_yes(meta$j_debug)) print(paste("IN j_db_put"))
-  db_writable <- 0 != file.access(meta$j_db_path, mode = 2) 
+  db_writable <- 0 == file.access(meta$j_db_path, mode = 2) 
   if (is_yes(meta$j_debug)) print(paste("db_writable:", db_writable))
   if (is_yes(meta$j_debug)) print(paste("j_db_path:", meta$j_db_path))
   if (!is_yes(meta$j_db) || !is.character(meta$j_db_table) || !is.character(meta$j_db_path) || !db_writable) return()
@@ -34,4 +34,11 @@ list_as_df <- function(lst) {
   lst_as_df <- as.data.frame(t(unlist(lapply(lst,toString))))
   rownames(lst_as_df) <- NULL
   lst_as_df
+}
+
+j_db_get_stats <- function(j_db_path = c('/Volumes/p_jamesgebruiker/stats/j_db.sqlite', 'M:/p_jamesgebruiker/stats/j_db.sqlite')[1], j_db_table = "jls") {
+  con <- DBI::dbConnect(RSQLite::SQLite(), dbname = j_db_path) # :dbname:
+  db_table        <- DBI::dbReadTable(con, j_db_table)
+  DBI::dbDisconnect(con)
+  return(db_table)
 }
