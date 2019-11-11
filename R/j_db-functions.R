beca418615092eb739fadca8bfff102173c3f15e
Martijn Dijkstra <m.dijkstra.cpb@gmail.com>
2019-11-11 15:46:29 +0100

    New stats features + added usernames

--- R/j_db-functions.R
+++ R/j_db-functions.R
@@ -6,9 +6,14 @@ j_db_put <- function(meta) { # meta$d0 is data
   if (!is_yes(meta$j_db) || !is.character(meta$j_db_table) || !is.character(meta$j_db_path) || !db_writable) return()
   con <- DBI::dbConnect(RSQLite::SQLite(), dbname = meta$j_db_path) # :dbname:
   if (is_yes(meta$j_debug)) print(paste("dbConnect"))
-  
+      
+  # Remove data
   this_data <- meta$d0
   meta$d0   <- NULL
+  
+  # Add extra meta data statistics
+  meta$j_db_put_timestamp <- unclass(Sys.time())
+  meta$j_db_put_path      <- getwd()
   
   meta_df <- list_as_df(meta)
   meta_df_fields <- colnames(meta_df)
@@ -40,5 +45,52 @@ j_db_get_stats <- function(j_db_path = c('/Volumes/p_jamesgebruiker/stats/j_db.s
   con <- DBI::dbConnect(RSQLite::SQLite(), dbname = j_db_path) # :dbname:
   db_table        <- DBI::dbReadTable(con, j_db_table)
   DBI::dbDisconnect(con)
-  return(db_table)
+  
+  mat <- as.data.frame(unique(cbind(user = db_table$user, timestamp = NA, path = db_table$j_db_put_path)), stringsAsFactors = F)
+  if (length(nrow(mat))) {
+    for (i in 1:nrow(mat)) {
+      index <- which(db_table$user == mat$user[i] & db_table$j_db_put_path == mat$path[i])
+      mat$timestamp[i] <- as.numeric(max(db_table$j_db_put_timestamp[index]))
+    }
+  
+    index <- sort(mat$timestamp, decreasing = T, index.return = TRUE)$ix
+    mat <- mat[index, ]
+
+    mat$timestamp <- as.POSIXct(as.numeric(mat$timestamp), origin = "1970-01-01")
+    
+    # Enrich user names with full names
+    un_path <- file.path(system.file("extdata", package = "james"), "usernames.RData")
+    if (file.exists(un_path)) {
+      un <- dget(un_path)
+      un_vec <- NULL
+      for (i in 1:nrow(mat)) {
+        un_vec[i] <- un[which(un[, "username"] == mat$user[i]), 2]
+      }
+      mat <- cbind(mat[, 1], un_vec, mat[, 2:ncol(mat)])
+    }
+    
+  }  
+  
+  return(mat)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
